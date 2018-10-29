cda7b0fcd3106d21171a25cd0db38edcea4c66ac
Maciek <mhubisz91@gmail.com>
2018-10-29 12:24:22 +0100

    maven + hibernate + postgres configs

--- src/main/java/backEnd/database/dbController.java
+++ src/main/java/backEnd/database/dbController.java
@@ -1,24 +1,18 @@
 package backEnd.database;
 
 
-
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-
 
 public class dbController {
 
     public dbController(){
         System.out.println("dzialam dbController");
         testConnection();
-        //Configuration.
+
 
     }
 
-    public void testConnection(){
+    private void testConnection(){
 
         System.out.println("Trying to create a test connection with the database.");
         Configuration configuration = new Configuration();
