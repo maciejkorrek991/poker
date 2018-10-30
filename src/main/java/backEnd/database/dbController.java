a2b1231f179a17688ad23594ffee683487a6356c
Maciek <mhubisz91@gmail.com>
2018-10-30 21:00:46 +0100

    dziala hibernate chyba

--- src/main/java/backEnd/database/dbController.java
+++ src/main/java/backEnd/database/dbController.java
@@ -6,9 +6,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class dbController {
@@ -21,31 +19,38 @@ public class dbController {
     }
 
     private void testConnection() {
-        /*String url = "jdbc:postgresql://localhost:5432/postgres";
-        Properties props = new Properties();
-        props.setProperty("user","postgres");
-        props.setProperty("password","admin");
-        props.setProperty("ssl","true");
+        /*
+        String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=admin&ssl=true";
+        Connection conn = null;
         try {
-            Connection conn = DriverManager.getConnection(url,props);
+            conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        Statement stmt = null;
+        stmt = conn.createStatement();
+        String sql = "select id_karty from game.cards";
+        ResultSet rs = stmt.executeQuery(sql);
+        //STEP 5: Extract data from result set
+        while(rs.next()){
+            //Retrieve by column name
+            int id  = rs.getInt("id_karty");
 
-         String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=admin&ssl=true";
-        try {
-            Connection conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
+
+            //Display values
+            System.out.print("ID: " + id);
+
+        }
+        rs.close();
+
+        */
+
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
 
         // create session
         Session session = factory.getCurrentSession();
 
         factory.close();
-
-
 
     }
 }
