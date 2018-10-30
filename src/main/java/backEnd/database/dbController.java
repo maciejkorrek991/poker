765c0de00b14e81898e9bf02e3ffe2ef89096143
Maciek <mhubisz91@gmail.com>
2018-10-30 20:45:15 +0100

    dziala hibernate chyba

--- src/main/java/backEnd/database/dbController.java
+++ src/main/java/backEnd/database/dbController.java
@@ -1,7 +1,15 @@
 package backEnd.database;
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class dbController {
 
@@ -12,14 +20,32 @@ public class dbController {
 
     }
 
-    private void testConnection(){
+    private void testConnection() {
+        /*String url = "jdbc:postgresql://localhost:5432/postgres";
+        Properties props = new Properties();
+        props.setProperty("user","postgres");
+        props.setProperty("password","admin");
+        props.setProperty("ssl","true");
+        try {
+            Connection conn = DriverManager.getConnection(url,props);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println("Trying to create a test connection with the database.");
-        Configuration configuration = new Configuration();
-        //configuration.configure("src/hibernate.cfg.xml");
-        //StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
-        /*SessionFactory sessionFactory = configuration.buildSessionFactory(ssrb.build());
-        Session session = sessionFactory.openSession();
-        System.out.println("Test connection with the database created successfuly.")*/
+         String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=admin&ssl=true";
+        try {
+            Connection conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        factory.close();
+
+
+
     }
 }
